ac84a6f68fe3adbdcf7c15c99f5d02b54180736b
George K <gkurkjian@myseneca.ca>
2024-11-04 18:44:55 -0500

    first commit/added Component folder

---
+++ my-app/pages/Components/SayHello.js
@@ -0,0 +1,7 @@
+export default function Hello() {
+    return (
+        <>
+            <p>Hello From SayHello.js component</p>
+        </>
+    )
+}
---
+++ my-app/pages/index.js
@@ -0,0 +1,11 @@
+import Hello from "./Components/SayHello"
+
+
+export default function Home() {
+  return (
+    <>
+      <p>Hello</p>
+      <Hello />
+    </>
+  );
+}
